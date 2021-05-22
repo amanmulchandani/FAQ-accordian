0775ca2d03ecd5b29008b342e8f4adcb3743c981
Aman Mulchandani <mulchandaniaman@gmail.com>
2021-05-22 17:33:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const questions = document.querySelectorAll(".question");
+const boldQuestion = document.querySelectorAll(".question-title");
+
+questions.forEach((question) => question.addEventListener("click", () => {
+
+    if(question.parentNode.classList.contains("active")){
+
+        question.parentNode.classList.toggle("active");
+    }
+    else{
+        questions.forEach(question => question.parentNode.classList.remove("active"));
+
+        question.parentNode.classList.add("active");
+    }
+
+}));
